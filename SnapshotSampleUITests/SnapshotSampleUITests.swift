97b7a06f88ebd0e9b63665e7e318d3604f3a496d
Tomoyuki Ito <ito@nagisa-inc.jp>
2018-04-05 19:36:56 +0900

    feat: uitest config

--- SnapshotSampleUITests/SnapshotSampleUITests.swift
+++ SnapshotSampleUITests/SnapshotSampleUITests.swift
@@ -20,6 +20,7 @@ class SnapshotSampleUITests: XCTestCase {
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         let app = XCUIApplication()
         setupSnapshot(app)
+        app.launchEnvironment["isUITesting"] = "true"
         app.launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
