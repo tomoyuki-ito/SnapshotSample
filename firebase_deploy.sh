28604e82a19f6450de1c141b1dea4d78d78fc421
Tomoyuki Ito <ito@nagisa-inc.jp>
2018-04-10 16:52:25 +0900

    ci: fix ci setting

---
+++ firebase_deploy.sh
@@ -0,0 +1,5 @@
+bundle exec fastlane snapshot
+cp fastlane/screenshots/screenshots.html functions/templates/index.html
+cp -r fastlane/screenshots/ja-JP public
+cd functions && npm i && cd ..
+firebase deploy --token $FIREBASE_TOKEN
