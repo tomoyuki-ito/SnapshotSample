e339879a7a563e0b4146a68a9482ff3c67e71a6b
Tomoyuki Ito <ito@nagisa-inc.jp>
2018-04-11 14:24:44 +0900

    ci: slck notification

--- firebase_deploy.sh
+++ firebase_deploy.sh
@@ -4,3 +4,4 @@ cp fastlane/screenshots/screenshots.html functions/templates/index.html
 cp -r fastlane/screenshots/ja-JP public
 cd functions && npm i && cd ..
 firebase deploy --token $FIREBASE_TOKEN
+curl -X POST --data-urlencode "payload={\"text\": \"<https://snapshotsample-6fe5e.firebaseapp.com/|Snapshot>を更新しました\"}" $SLACK_WEBHOOK_URL
